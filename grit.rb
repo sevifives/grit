84fc8b9855f4521e2b980ceaa00cf5a2a9c2de4e
Jason Dooley <jd@sevif.me>
2011-12-22 11:38:17 -0500

    Grit it.

---
+++ grit.rb
@@ -0,0 +1,68 @@
+#!/usr/bin/env ruby
+
+# Grit is a way to manage multiple repos in a git repository seamlessly
+# using the git CL tool
+
+# What Grit should do:
+# - Proxy the git API
+# - Don't get in the way
+# - Allow the user to make the normal git choices due to that proxy
+# - Not require a bunch of retarded crap like Google's git-repo to get started
+
+# Sample config.yml
+# --- 
+# :root: /Users/john/my_project
+# :repositories: 
+#   - :name: Sproutcore
+#     :path: frameworks/sproutcore
+#   - :name: SCUI
+#     :path: frameworks/scui
+
+
+require 'yaml'
+require 'fileutils'
+
+class Grit
+  def initialize_grit (args)
+    location = args[0] || Dir.pwd
+
+    if File.directory?(location)
+      directory = File.join(location,'.grit')
+      FileUtils.mkdir(directory) unless File.directory?(directory)
+
+      config_file = directory+'/config.yml'
+      if !File.exists?(config_file)
+        config = {}
+        config[:root] ||= Dir.pwd
+        config[:repositories] ||= []
+
+        open(directory+'/config.yml','w') {|f| YAML.dump(config,f)}
+      end
+    else
+      puts "Directory doesn't exist!"
+    end
+  end
+
+  def proceed (args)
+    config = open(File.join(FileUtils.pwd,'.grit/config.yml')) {|f| YAML.load(f) }
+    repositories = config[:repositories].unshift({:name => 'Root',:path => config[:root]})
+
+    to_do = args.join(' ')
+    puts repositories.inspect
+    repositories.each do |repo|
+      puts "Performing operation #{to_do} on #{repo[:name]}"
+      raise "Shit!" if path.nil?
+      Dir.chdir(repo[:path]) do |d|
+        puts `git #{to_do}`
+      end
+    end
+  end
+end
+
+
+project = Grit.new
+if ARGV[0] === 'init'
+  project.initialize_grit(ARGV[1..-1])
+else
+  project.proceed ARGV
+end
