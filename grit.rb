e26c79506eef14ce59e2f70b1cd1ec9476f7532d
sevifives <jdooley@kungfeud.com>
2021-07-13 12:28:06 -0700

    Update grit.rb
    Not not

--- grit.rb
+++ grit.rb
@@ -105,7 +105,7 @@ class Grit
 
   def get_current_profile
     location = Dir.pwd
-    if (!File.exists?(".grit/current_profile"))
+    if (File.exists?(".grit/current_profile"))
       return File.read(".grit/current_profile")  
     end
     return "config"
